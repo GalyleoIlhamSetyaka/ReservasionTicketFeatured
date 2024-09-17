69d4c11c1bc9949fc4ab0ec60118515e8eb11a69
Galyleo Ilham Setyaka <92193398+GalyleoIlhamSetyaka@users.noreply.github.com>
2024-09-17 15:30:37 +0700

    Update pemesanan.dart

--- lib/pemesanan.dart
+++ lib/pemesanan.dart
@@ -167,6 +167,48 @@ class _FormPageState extends State<FormPage> {
                 width: 200,
               ),
               SizedBox(height: 16.0),
+                            TextFormField(
+                controller: _controller4,
+                readOnly: true,
+                decoration: InputDecoration(
+                  labelText: 'Upload Bukti Pembayaran (JPG)',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.image),
+                    onPressed: () => _selectImage(context),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+                Card(
+                  elevation: 4,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Scan QRIS untuk Pembayaran',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 12),
+                        Image.asset(
+                          'assets/qrcode.png',  // Pastikan gambar ini ada di folder assets
+                          height: 200,
+                          width: double.infinity,
+                          fit: BoxFit.contain,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Silakan scan kode QR di atas untuk melakukan pembayaran.',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               SizedBox(height: 32.0),
               Center(
                 child: ElevatedButton(
